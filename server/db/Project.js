2c854f456cd979572aaa74aea73d76f753250c9d
Elaine Koppel <ejk416@gmail.com>
2023-06-12 18:25:55 -0500

    project task models

---
+++ server/db/Project.js
@@ -0,0 +1,55 @@
+const conn = require('./conn');
+const { STRING, UUID, UUIDV4, TEXT, BOOLEAN, ENUM } = conn.Sequelize;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT;
+
+
+const Project = conn.define('project', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  startDate: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  deadline: {
+    type: STRING,
+    allowNull: false, 
+    validate: {
+      notEmpty: true,
+    }
+  },
+  priority: {
+    type: ENUM("Low", "High"),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  userStatus: {
+    type: ENUM("0% completed", "25% completed", "50% completed", "75% completed", "100% completed"),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  notes: {
+    type: TEXT
+  },
+  // assignedUser: {
+  //   allowNull: false,
+  //   validate: {
+  //     notEmpty: true,
+  //   }
+  // },
+});
+
+
+
+module.exports = Project;
